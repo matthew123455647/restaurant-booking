ddb6b0c78e9ac8b2b0ca78cd9071c99ca7a9441a
Jerlyn <jerlynho00@gmail.com>
2023-11-25 07:47:28 +0800

    added testing codes for registerTests.test.js with coverage

--- tests/registerTests.test.js
+++ tests/registerTests.test.js
@@ -1,87 +1,304 @@
-// const { describe, it } = require("mocha");
-// const { expect } = require("chai");
-// const sinon = require("sinon");
-// const fs = require("fs").promises;
-// const { register } = require("../utils/UserUtil");
+const fs = require("fs").promises;
+const { describe, it, beforeEach, afterEach } = require("mocha");
+const { expect } = require("chai");
+const sinon = require("sinon");
+const {
+  register,
+  readJSON,
+  writeJSON,
+  isValidDate,
+} = require("../utils/UserUtil");
 
-// describe("Testing User Registration", () => {
-//   let writeFileStub;
+describe("Testing Register Function", () => {
+  let readFileStub;
+  let writeFileStub;
 
-//   beforeEach(() => {
-//     // Stub the fs.writeFile method to prevent writing to the file during tests
-//     writeFileStub = sinon.stub(fs, "writeFile");
-//   });
+  beforeEach(() => {
+    // Stub the fs.readFile and fs.writeFile methods
+    readFileStub = sinon.stub(fs, "readFile");
+    writeFileStub = sinon.stub(fs, "writeFile");
+  });
 
-//   afterEach(() => {
-//     // Restore the original behavior of fs.writeFile after each test
-//     writeFileStub.restore();
-//   });
+  afterEach(() => {
+    // Restore the stubbed methods to their original implementations
+    readFileStub.restore();
+    writeFileStub.restore();
+  });
 
-//   it("Should successfully register a user with valid data", async () => {
-//     const req = {
-//       body: {
-//         email: "test@example.com",
-//         password: "Password123!",
-//         birthday: "1990-01-01",
-//         first_name: "John",
-//         last_name: "Doe",
-//         phone_number: "12345678",
-//         profile_picture: "https://example.com/profile.jpg",
-//         username: "johndoe",
-//       },
-//     };
+  it("Should register a new user successfully", async () => {
+    const req = {
+      body: {
+        email: "test@example.com",
+        password: "Password123!",
+        birthday: "1990-01-01",
+        first_name: "John",
+        last_name: "Doe",
+        phone_number: "12345678",
+        profile_picture: "https://example.com/profile.jpg",
+        username: "johndoe",
+      },
+    };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
 
-//     const res = {
-//       status: function (code) {
-//         expect(code).to.equal(201);
-//         return this;
-//       },
-//       json: function (data) {
-//         expect(data).to.be.an("array").with.lengthOf(1);
-//         // You might want to add more specific assertions based on the expected user object structure
-//       },
-//     };
+    // Simulate reading an empty users.json file
+    readFileStub.resolves("[]");
 
-//     await register(req, res);
+    // Simulate writing the user data to the file
+    writeFileStub.resolves(
+      '[{"email":"test@example.com","password":"Password123!","birthday":"1990-01-01","first_name":"John","last_name":"Doe","phone_number":"12345678","profile_picture":"https://example.com/profile.jpg","username":"johndoe"}]'
+    );
 
-//     // Check if fs.writeFile was called with the expected arguments
-//     sinon.assert.calledOnceWithExactly(
-//       writeFileStub,
-//       "utils/users.json",
-//       sinon.match.string,
-//       "utf8"
-//     );
-//   });
+    await register(req, res);
 
-//   it("Should handle validation error for invalid email", async () => {
-//     const req = {
-//       body: {
-//         email: "invalid-email",
-//         password: "Password123!",
-//         birthday: "1990-01-01",
-//         first_name: "John",
-//         last_name: "Doe",
-//         phone_number: "12345678",
-//         profile_picture: "https://example.com/profile.jpg",
-//         username: "johndoe",
-//       },
-//     };
+    expect(res.status.calledWith(201)).to.be.true;
+    expect(res.json.calledOnce).to.be.true;
+    expect(
+      res.json.calledWith([
+        {
+          /* expected user data */
+        },
+      ])
+    ).to.be.true;
+  });
 
-//     const res = {
-//       status: function (code) {
-//         expect(code).to.equal(500);
-//         return this;
-//       },
-//       json: function (data) {
-//         expect(data.message).to.equal(
-//           "Validation error: Invalid email or password"
-//         );
-//       },
-//     };
+  it("Should handle validation error for invalid email or password", async () => {
+    const req = {
+      body: {
+        email: "invalid-email",
+        password: "weak",
+        birthday: "1990-01-01",
+        first_name: "John",
+        last_name: "Doe",
+        phone_number: "12345678",
+        profile_picture: "https://example.com/profile.jpg",
+        username: "johndoe",
+      },
+    };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
 
-//     await register(req, res);
-//     sinon.assert.notCalled(writeFileStub); // Ensure fs.writeFile was not called
-//   });
+    await register(req, res);
 
-//   // Add more test cases for other validation scenarios...
-// });
+    expect(res.status.calledWith(500)).to.be.true;
+    expect(res.json.calledOnce).to.be.true;
+    expect(
+      res.json.calledWith({
+        message: "Validation error: Invalid email or password",
+      })
+    ).to.be.true;
+  });
+
+  it("Should handle validation error for invalid username", async () => {
+    const req = {
+      body: {
+        email: "test@example.com",
+        password: "StrongPassword1!",
+        birthday: "1990-01-01",
+        first_name: "John",
+        last_name: "Doe",
+        phone_number: "12345678",
+        profile_picture: "https://example.com/profile.jpg",
+        username: "in",
+      },
+    };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await register(req, res);
+
+    expect(res.status.calledWith(500)).to.be.true;
+    expect(res.json.calledOnce).to.be.true;
+    expect(
+      res.json.calledWith({
+        message:
+          "Validation error: Username must contain only lowercase letters and be at least 3 characters long",
+      })
+    ).to.be.true;
+  });
+
+  it("Should handle validation error for invalid profile picture URL", async () => {
+    const req = {
+      body: {
+        email: "test@example.com",
+        password: "StrongPassword1!",
+        birthday: "1990-01-01",
+        first_name: "John",
+        last_name: "Doe",
+        phone_number: "12345678",
+        profile_picture: "invalid-url",
+        username: "johndoe",
+      },
+    };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await register(req, res);
+
+    expect(res.status.calledWith(500)).to.be.true;
+    expect(res.json.calledOnce).to.be.true;
+    expect(
+      res.json.calledWith({
+        message: "Validation error: Invalid profile picture URL",
+      })
+    ).to.be.true;
+  });
+
+  it("Should handle validation error for age below 18", async () => {
+    const req = {
+      body: {
+        email: "test@example.com",
+        password: "StrongPassword1!",
+        birthday: "2023-01-01", // Future date to ensure age is below 18
+        first_name: "John",
+        last_name: "Doe",
+        phone_number: "12345678",
+        profile_picture: "https://example.com/profile.jpg",
+        username: "johndoe",
+      },
+    };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await register(req, res);
+
+    expect(res.status.calledWith(500)).to.be.true;
+    expect(res.json.calledOnce).to.be.true;
+    expect(
+      res.json.calledWith({
+        message:
+          "Validation error: You must be at least 18 years old to register",
+      })
+    ).to.be.true;
+  });
+
+  it("Should handle validation error for invalid phone number", async () => {
+    const req = {
+      body: {
+        email: "test@example.com",
+        password: "StrongPassword1!",
+        birthday: "1990-01-01",
+        first_name: "John",
+        last_name: "Doe",
+        phone_number: "invalid-number",
+        profile_picture: "https://example.com/profile.jpg",
+        username: "johndoe",
+      },
+    };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await register(req, res);
+
+    expect(res.status.calledWith(500)).to.be.true;
+    expect(res.json.calledOnce).to.be.true;
+    expect(
+      res.json.calledWith({
+        message: "Validation error: Phone number must be exactly 8 digits long",
+      })
+    ).to.be.true;
+  });
+
+  it("Should handle validation error for invalid password", async () => {
+    const req = {
+      body: {
+        email: "test@example.com",
+        password: "weakpassword",
+        birthday: "1990-01-01",
+        first_name: "John",
+        last_name: "Doe",
+        phone_number: "12345678",
+        profile_picture: "https://example.com/profile.jpg",
+        username: "johndoe",
+      },
+    };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await register(req, res);
+
+    expect(res.status.calledWith(500)).to.be.true;
+    expect(res.json.calledOnce).to.be.true;
+    expect(
+      res.json.calledWith({
+        message:
+          "Validation error: Password must contain at least one capital letter and one of the specified special characters",
+      })
+    ).to.be.true;
+  });
+
+  it("Should handle validation error for invalid birthdate or age over 116", async () => {
+    const req = {
+      body: {
+        email: "test@example.com",
+        password: "StrongPassword1!",
+        birthday: "1800-01-01", // More than 116 years ago
+        first_name: "John",
+        last_name: "Doe",
+        phone_number: "12345678",
+        profile_picture: "https://example.com/profile.jpg",
+        username: "johndoe",
+      },
+    };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await register(req, res);
+
+    expect(res.status.calledWith(400)).to.be.true;
+    expect(res.json.calledOnce).to.be.true;
+    expect(
+      res.json.calledWith({
+        message:
+          "Validation error: Invalid birthdate or maximum age limit exceeded (116 years)",
+      })
+    ).to.be.true;
+  });
+
+  it("Should handle unexpected error", async () => {
+    const req = {
+      body: {
+        email: "test@example.com",
+        password: "StrongPassword1!",
+        birthday: "1990-01-01",
+        first_name: "John",
+        last_name: "Doe",
+        phone_number: "12345678",
+        profile_picture: "https://example.com/profile.jpg",
+        username: "johndoe",
+      },
+    };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    // Simulate an unexpected error during registration
+    sinon.stub(fs, "readFile").rejects(new Error("Unexpected error"));
+
+    await register(req, res);
+
+    expect(res.status.calledWith(500)).to.be.true;
+    expect(res.json.calledOnce).to.be.true;
+    expect(res.json.calledWith({ message: "Unexpected error" })).to.be.true;
+
+    // Restore the stubbed method
+    fs.readFile.restore();
+  });
+});
