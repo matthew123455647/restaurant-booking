1a04e6f35cf5087fba9e0f21679ed0ab80de76c7
Jerlyn <jerlynho00@gmail.com>
2023-11-27 02:57:49 +0800

    added abit more for login

--- utils/LoginUtil.js
+++ utils/LoginUtil.js
@@ -16,12 +16,22 @@ async function login(req, res) {
     const email = req.body.email;
     const password = req.body.password;
     const allUsers = await readJSON("./utils/users.json");
+
+    if (!email || !password) {
+      return res.status(500).json({ message: "Invalid credentials!" });
+    }
+
     var validCredentials = false;
     for (var i = 0; i < allUsers.length; i++) {
       var currUser = allUsers[i];
-      if (currUser.email == email && currUser.password == password)
+
+      if (
+        currUser.email == email &&
+        currUser.password.toLowerCase() == password.toLowerCase()
+      )
         validCredentials = true;
     }
+
     if (validCredentials) {
       return res.status(201).json({ message: "Login successful!" });
     } else {
@@ -31,6 +41,7 @@ async function login(req, res) {
     return res.status(500).json({ message: error.message });
   }
 }
+
 module.exports = {
   readJSON,
   login,
