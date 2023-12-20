cba666fa2c1ee2ce3f0c0e63b79b932f5faaacbb
matthew123455647 <chanmatthewkl@gmail.com>
2023-12-20 15:17:16 +0800

    I have did all the frontend

--- public/js/restaurant.js
+++ public/js/restaurant.js
@@ -34,11 +34,11 @@ function displayRestaurants() {
         var ratingNumber = parseFloat(rating).toFixed(1); // Convert and round the rating to one decimal place
 
         // Updated image size
-        var cell = '<div class="card" style="width: 18%; height: 100%; display: flex; flex-direction: column; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); border-radius: 8px; margin: 10px; margin-left: 40px; margin-right: 20px; overflow: hidden;">' +
+        var cell = '<div class="card" item="' + count + '" style="width: 18%; height: 100%; display: flex; flex-direction: column; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); border-radius: 8px; margin: 10px; margin-left: 40px; margin-right: 20px; overflow: hidden;">' +
         '<img class="card-img-top" src="' + thumbnail + '" alt="Card image cap" style="width: 100%; height: 150px; object-fit: cover;">' +
         '<div class="rating-pill" style="align-self: flex-end; padding: 5px 10px; background-color: #FFD700; color: #000; border-radius: 5px; font-weight: bold;">' + ratingNumber + '</div>' +
         '<div class="card-body" style="margin-bottom: 10px;">' +
-            '<h5 style="cursor: pointer; color: #333; font-weight: bold; margin: 0;" class="card-title" onclick="viewOneRest(\'' + (JSON.stringify(restaurant_array[count]).replaceAll('\"', '&quot;')).replaceAll('\'', '&apos;')  + '\')">' + title + '</h5>' +
+            '<h5 style="cursor: pointer; color: #333; font-weight: bold; margin: 0;" class="card-title" onclick="viewOneRest(\'' + (JSON.stringify(restaurant_array[count]).replaceAll('\"', '&quot;')).replaceAll('\'', '&apos;')  + '\'); showRestaurantReviews(' + count + ');">' + title + '</h5>' +
             '<p class="card-text" style="color: #777;">' + locations + '</p>' +
         '</div>' +
     '</div>';
@@ -51,7 +51,7 @@ function displayRestaurants() {
     }
 }
 
-
+var currentIndex = 0;
 function viewOneRest(data) {
     var selectedResource = JSON.parse(data);
     console.log(selectedResource)
@@ -60,13 +60,21 @@ function viewOneRest(data) {
     document.getElementById("operatingHours").innerHTML = selectedResource.operatingHours;
     document.getElementById("rating").innerHTML = selectedResource.rating;
     document.getElementById("image").src= selectedResource.image;
-    document.getElementById("review").innerHTML = selectedResource.review;
+    //document.getElementById("review").innerHTML = selectedResource.review;
+
     document.getElementById("locations").innerHTML = selectedResource.locations;
     document.getElementById("contactNo").innerHTML = selectedResource.contactNo;
 
 
+    currentIndex = selectedResource._id;
+    console.log("...." + currentIndex)
+    
+
+
     $('#restaurantsModal').modal('show');
     }
+
+
 
 function getRestaurantsByName(restaurantName) {
     //var restaurantName = JSON.parse(sessionStorage.getItem("restaurant"));
@@ -89,8 +97,7 @@ function getRestaurantsByName(restaurantName) {
           document.getElementById("locations").innerHTML = response.locations;
           document.getElementById("contactNo").innerHTML = response.contactNo;
           document.getElementById("review").innerHTML = response.review;
-          
-          
+         
           
           
       
--- public/js/review.js
+++ public/js/review.js
@@ -2,7 +2,7 @@ function fetchReviews() {
     var request = new XMLHttpRequest();
 
     request.open('GET', "/comment", true);
-    request.setRequestHeader('Content-Type', 'comment/json');
+    request.setRequestHeader('Content-Type', 'application/json');
     //This command starts the calling of the comments api
     request.onload = function () {
         //get all the comments records into our comments array
@@ -15,16 +15,19 @@ function fetchReviews() {
 
 //This function is to display all the comments of that movie
 //whenever the user click on the "comment" button
-function showRestaurantReviews(element) {
-    document.getElementById("emptyComment").innerHTML = "No review yet. Create one now";
-    var item = element.getAttribute("item");
-    currentIndex = item;
+function showRestaurantReviews(item) {
+    document.getElementById("review").innerHTML = "No review yet. Create one now";
+    // var item = element.getAttribute("item");
+    // currentIndex = item;
     document.getElementById("review").textContent = "Review for " + restaurant_array[item].restaurantName;
-    document.getElementById("commentBody").textContent = "";
+    document.getElementById("review").textContent = "";
 
     for (var i = 0; i < review_array.length; i++) {
-        if (review_array[i].restaurant_id === restaurant_array[item]._id) {
-            document.getElementById("emptyComment").innerHTML = "";
+        if (review_array[i].restaurantName === restaurant_array[item].restaurantName) {
+            console.log("found")
+console.log(review_array[i])
+console.log(restaurant_array[item]._id)
+
             selectedRestaurant_id = restaurant_array[item]._id;
             star = "";
             var html = '<div class="text-center" style="width:100%;">                                                           \
@@ -35,15 +38,15 @@ function showRestaurantReviews(element) {
                                 </div>                                                                                          \
                             </div>                                                                                              \
                         </div>";
-            document.getElementById("commentBody").insertAdjacentHTML('beforeend', html);
+            document.getElementById("review").insertAdjacentHTML('beforeend', html);
 
             var star = "";
             for (var j = 0; j < review_array[i].rating; j++) {
                 console.log(i);
                 star += "<img src='images/popcorn.png' style='width:50px' />";
             }
-            star += "<i class='far fa-trash-alt fa-2x edit' data-dismiss='modal' item='" + i + "' onClick='deleteReview(this)' ></i>";
-            star += "<i class='far fa-edit fa-2x edit' data-toggle='modal' data-target='#editReviewModal' data-dismiss='modal' item='" + i + "' onClick='editReview(this)' ></i>";
+            // star += "<i class='far fa-trash-alt fa-2x edit' data-dismiss='modal' item='" + i + "' onClick='deleteReview(this)' ></i>";
+            // star += "<i class='far fa-edit fa-2x edit' data-toggle='modal' data-target='#editReviewModal' data-dismiss='modal' item='" + i + "' onClick='editReview(this)' ></i>";
             document.getElementById("rating" + i).insertAdjacentHTML('beforebegin', star + "<br/>");
         }
     }
@@ -58,21 +61,23 @@ function newReview() {
 // Submit or send the new comment to the server to be added.
 function addReview() {
     var review = new Object();
-    review.restaurant_id = restaurant_array[currentIndex]._id;
-    review.user_id = 
+    review.restaurantName = restaurant_array[currentIndex].restaurantName;
+
     console.log(review);
     review.username = document.getElementById("username1").value;
     review.review = document.getElementById("userComments").value;
     review.dateOfVisit = document.getElementById("dateOfVisit").value;
-    review.image4 = null;
-    review.timestamp = null;
+
+    //review.timestamp = document.getElementById("timestamp").value;
     review.rating = rating;
+
+    console.log(review)
     
     var postReview = new XMLHttpRequest(); // new HttpRequest instance to send comment
 
     postReview.open("POST", "/comment", true);
 
-    postReview.setRequestHeader("Content-Type", "comment/json");
+    postReview.setRequestHeader("Content-Type", "application/json");
     postReview.onload = function () {
         console.log("new review sent");
         fetchReviews();
@@ -92,7 +97,7 @@ function rateIt(element) {
     // This is another way of writing 'for' loop, which initialises the 
     // popcorn images to use black and white.
     for (let popcorn of popcorns) {
-        popcorn.setAttribute("src", popcornBWImage);
+        popcorn.setAttribute("src", "images/popcorn_bw.png");
     }
     changePopcornImage(num, classTarget);
 }
@@ -102,33 +107,33 @@ function rateIt(element) {
 function changePopcornImage(num, classTarget) {
     switch (eval(num)) {
         case 1:
-            document.querySelector(classTarget + "[value='1']").setAttribute("src", popcornImage);
+            document.querySelector(classTarget + "[value='1']").setAttribute("src", "images/popcorn.png");
             rating = 1;
             break;
         case 2:
-            document.querySelector(classTarget + "[value='1']").setAttribute("src", popcornImage);
-            document.querySelector(classTarget + "[value='2']").setAttribute("src", popcornImage);
+            document.querySelector(classTarget + "[value='1']").setAttribute("src", "images/popcorn.png");
+            document.querySelector(classTarget + "[value='2']").setAttribute("src", "images/popcorn.png");
             rating = 2;
             break;
         case 3:
-            document.querySelector(classTarget + "[value='1']").setAttribute("src", popcornImage);
-            document.querySelector(classTarget + "[value='2']").setAttribute("src", popcornImage);
-            document.querySelector(classTarget + "[value='3']").setAttribute("src", popcornImage);
+            document.querySelector(classTarget + "[value='1']").setAttribute("src", "images/popcorn.png");
+            document.querySelector(classTarget + "[value='2']").setAttribute("src", "images/popcorn.png");
+            document.querySelector(classTarget + "[value='3']").setAttribute("src","images/popcorn.png");
             rating = 3;
             break;
         case 4:
-            document.querySelector(classTarget + "[value='1']").setAttribute("src", popcornImage);
-            document.querySelector(classTarget + "[value='2']").setAttribute("src", popcornImage);
-            document.querySelector(classTarget + "[value='3']").setAttribute("src", popcornImage);
-            document.querySelector(classTarget + "[value='4']").setAttribute("src", popcornImage);
+            document.querySelector(classTarget + "[value='1']").setAttribute("src", "images/popcorn.png");
+            document.querySelector(classTarget + "[value='2']").setAttribute("src", "images/popcorn.png");
+            document.querySelector(classTarget + "[value='3']").setAttribute("src", "images/popcorn.png");
+            document.querySelector(classTarget + "[value='4']").setAttribute("src", "images/popcorn.png");
             rating = 4;
             break;
         case 5:
-            document.querySelector(classTarget + "[value='1']").setAttribute("src", popcornImage);
-            document.querySelector(classTarget + "[value='2']").setAttribute("src", popcornImage);
-            document.querySelector(classTarget + "[value='3']").setAttribute("src", popcornImage);
-            document.querySelector(classTarget + "[value='4']").setAttribute("src", popcornImage);
-            document.querySelector(classTarget + "[value='5']").setAttribute("src", popcornImage);
+            document.querySelector(classTarget + "[value='1']").setAttribute("src", "images/popcorn.png");
+            document.querySelector(classTarget + "[value='2']").setAttribute("src", "images/popcorn.png");
+            document.querySelector(classTarget + "[value='3']").setAttribute("src", "images/popcorn.png");
+            document.querySelector(classTarget + "[value='4']").setAttribute("src", "images/popcorn.png");
+            document.querySelector(classTarget + "[value='5']").setAttribute("src", "images/popcorn.png");
             rating = 5;
             break;
     }
