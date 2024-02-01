9fd913c7feeb5e94d800cb9ccbdd78a602caae80
matthew123455647 <chanmatthewkl@gmail.com>
2024-02-01 12:04:39 +0800

    I have made some changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+const logger = require('./logger');
 
 const PORT = process.env.PORT || 5050;
 var startPage = "index.html";
@@ -9,20 +10,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
 
+const statusMonitor = require('express-status-monitor');
+app.use(statusMonitor());
+
 
 
 const { viewComment } = require("./utils/CommentUtil");
 const { addComment } = require("./utils/AddCommentUtil");
-const { viewRestaurant  } = require('./utils/RestaurantUtil');
+const { viewRestaurant } = require('./utils/RestaurantUtil');
 const { register } = require("./utils/UserUtil");
 const { login } = require("./utils/LoginUtil");
 const { viewBooking } = require('./utils/ViewBookingUtil');
-const { addBooking} = require('./utils/AddBookingUtil');
+const { addBooking } = require('./utils/AddBookingUtil');
 const { deleteBooking } = require('./utils/DeleteBookingUtil')
 
 //Comment GET, PUSH
 app.get("/comment", viewComment);
-app.post("/comment" , addComment);
+app.post("/comment", addComment);
 //Restaurant GET, PUSH
 
 app.get("/restaurant", viewRestaurant);
@@ -42,6 +46,8 @@ app.get("/", (req, res) => {
 
 const server = app.listen(PORT, function () {
   console.log(`Demo project at: ${PORT}!`)
+  logger.info(`Demo project at: ${PORT}!`);
+  logger.error(`Example or error log`)
 });
-  
+
 module.exports = { app, server }
